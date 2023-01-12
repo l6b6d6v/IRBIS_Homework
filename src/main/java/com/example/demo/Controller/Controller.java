a5f693018bab1e8c085f99fd92bce3a87f30afd7
l6b6d6v <50709384+l6b6d6v@users.noreply.github.com>
2023-01-12 16:25:52 +0400

    Initial commit Alpha 0.0.1

---
+++ src/main/java/com/example/demo/Controller/Controller.java
@@ -0,0 +1,22 @@
+package com.example.demo.Controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @Value("${spring.application.name}")
+    private String name;
+
+    @GetMapping("/name")
+    public String getNameApplication() {
+        return name;
+    }
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello World";
+    }
+}
